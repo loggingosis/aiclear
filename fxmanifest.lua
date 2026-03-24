55af704d24b48bb2250b9e5968b41856fa77ef9e
Pharma <34245836+logansportfolio@users.noreply.github.com>
2026-03-23 21:06:03 -0500

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Logan'
-description 'Delete all ambient AI peds and NPC vehicles with command'
-version '1.0.0'
-
-client_script 'client.lua'
-server_script 'server.lua'
